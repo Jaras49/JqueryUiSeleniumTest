147565889abf55049cd6f1f50a3d959bf4a70168
jgodwin <jaras48@gmail.com>
2018-10-23 21:11:11 +0200

    CalendarTest refactor. CalendarPage added

---
+++ src/main/java/com/jqueryui/calendar/CalendarPage.java
@@ -0,0 +1,103 @@
+package com.jqueryui.calendar;
+
+import com.jqueryui.menu.LeftMenu;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class CalendarPage {
+
+    private WebDriver driver;
+    private LeftMenu leftMenu;
+
+    @FindBy(css = ".demo-frame")
+    private WebElement iFrame;
+
+    @FindBy(css = "#ui-datepicker-div")
+    private WebElement calendarDiv;
+
+    @FindBy(css = "#datepicker")
+    private WebElement inputField;
+
+    @FindBy(css = "div .ui-datepicker-month")
+    private WebElement currentMonthElement;
+
+    @FindBy(css = "div .ui-datepicker-year")
+    private WebElement currentYearElement;
+
+    @FindBy(css = "div .ui-datepicker-next")
+    private WebElement callendarNext;
+
+    @FindBy(css = "div .ui-datepicker-prev")
+    private WebElement callendarPrev;
+
+    @FindBy(css = "td[data-handler='selectDay']")
+    private List<WebElement> calendarDays;
+
+    public CalendarPage(WebDriver driver, LeftMenu leftMenu) {
+        this.driver = driver;
+        this.leftMenu = leftMenu;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void switchToIframe() {
+        driver.switchTo().frame(iFrame);
+    }
+
+    public void switchToParentFrame() {
+        driver.switchTo().parentFrame();
+    }
+
+    public void clickInputField() {
+        inputField.click();
+    }
+
+    public void clickCallendarNext() {
+        callendarNext.click();
+    }
+
+    public void clickCallendarPrev() {
+        callendarPrev.click();
+    }
+
+    public void moveToDate(LocalDate date) {
+        LocalDate desiredYearAndMonth = LocalDate.of(date.getYear(), date.getMonth(), 1);
+        LocalDate currentYearAndMonth = getCurrentYearAndMonth();
+
+        while (!desiredYearAndMonth.equals(currentYearAndMonth)) {
+            if (desiredYearAndMonth.isAfter(currentYearAndMonth)) {
+                clickCallendarNext();
+            } else if (desiredYearAndMonth.isBefore(currentYearAndMonth)) {
+                clickCallendarPrev();
+            }
+            currentYearAndMonth = getCurrentYearAndMonth();
+        }
+    }
+
+    public void clickDay(LocalDate date) {
+        calendarDays.stream()
+                .filter(day -> Integer.valueOf(day.getAttribute("data-month")).equals(date.getMonthValue() - 1))
+                .map(day -> day.findElement(By.cssSelector("a")))
+                .filter(day -> Integer.valueOf(day.getText()).equals(date.getDayOfMonth()))
+                .findAny()
+                .ifPresent(WebElement::click);
+    }
+
+    public LeftMenu getLeftMenu() {
+        return leftMenu;
+    }
+
+    public String getInputValue() {
+        return inputField.getAttribute("value");
+    }
+
+    private LocalDate getCurrentYearAndMonth() {
+        return LocalDate.of(Integer.valueOf(currentYearElement.getText()), Month.valueOf(currentMonthElement.getText().toUpperCase()), 1);
+    }
+}
--- src/test/java/jqueryui/calendar/CalendarTest.java
+++ src/test/java/jqueryui/calendar/CalendarTest.java
@@ -1,111 +1,48 @@
 package jqueryui.calendar;
 
+import com.jqueryui.calendar.CalendarPage;
+import com.jqueryui.menu.LeftMenu;
 import jqueryui.AbstractTest;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
 public class CalendarTest extends AbstractTest {
 
     private static final String CALENDAR_URL = "https://jqueryui.com/datepicker/#other-months";
-    private static final String DATE_FORMAT = "d.MM.yyyy";
+    private static final String INPUT_DATE_FORMAT = "d.MM.yyyy";
+    private static final String OUTPUT_DATE_FORMAT = "MM/dd/yyyy";
 
     @Override
     @BeforeMethod
-    public void setUp() throws InterruptedException {
+    public void setUp() {
         super.setUp();
         driver.get(CALENDAR_URL);
-        WebElement iframeElement = driver.findElement(By.className("demo-frame"));
-        Thread.sleep(3000);
-        driver.switchTo().frame(iframeElement);
     }
 
     @DataProvider(name = "calendarData")
     public static Object[][] calendarData() {
         return new Object[][]{{"1.02.2019", "02/01/2019"},
-                {"today", LocalDate.now().format(DateTimeFormatter.ofPattern("MM/dd/yyyy"))},
+                {LocalDate.now().format(DateTimeFormatter.ofPattern(INPUT_DATE_FORMAT)),
+                        LocalDate.now().format(DateTimeFormatter.ofPattern(OUTPUT_DATE_FORMAT))},
                 {"06.07.2018", "07/06/2018"},
                 {"20.11.2017", "11/20/2017"}
         };
     }
 
     @Test(dataProvider = "calendarData")
-    public void datePickerTest(String date, String expected) {
+    public void calendarTest(String date, String expected) throws InterruptedException {
+        CalendarPage calendarPage = new CalendarPage(driver, new LeftMenu(driver));
+        calendarPage.switchToIframe();
+        calendarPage.clickInputField();
+        calendarPage.moveToDate(LocalDate.parse(date, DateTimeFormatter.ofPattern(INPUT_DATE_FORMAT)));
+        calendarPage.clickDay(LocalDate.parse(date, DateTimeFormatter.ofPattern(INPUT_DATE_FORMAT)));
 
-        LocalDate parsedDate = parseDate(date);
-        LocalDate desiredYearAndMonth = LocalDate.of(parsedDate.getYear(), parsedDate.getMonth(), 1);
-
-        openCalendar();
-
-        WebElement calendarDiv = driver.findElement(By.cssSelector("#ui-datepicker-div"));
-
-        iterateToYearAndMonth(calendarDiv, desiredYearAndMonth);
-        clickDate(parsedDate, calendarDiv, desiredYearAndMonth);
-
-        WebElement dateInput = driver.findElement(By.cssSelector("#datepicker"));
-        assertEquals(dateInput.getAttribute("value"), expected);
-    }
-
-    private void iterateToYearAndMonth(WebElement calendarDiv, LocalDate desiredYearAndMonth) {
-        LocalDate currentYearAndMonth = getCurrentYearAndMonth(calendarDiv);
-
-        String actionSelector = getActionSelector(desiredYearAndMonth, currentYearAndMonth);
-
-        while (!desiredYearAndMonth.equals(currentYearAndMonth)) {
-            WebElement nextElement = calendarDiv.findElement(By.cssSelector(actionSelector));
-            nextElement.click();
-            currentYearAndMonth = getCurrentYearAndMonth(calendarDiv);
-        }
-    }
-
-    private LocalDate parseDate(String date) {
-        if (date.equalsIgnoreCase("today")) {
-            return LocalDate.now();
-        }
-        return LocalDate.parse(date, DateTimeFormatter.ofPattern(DATE_FORMAT));
-    }
-
-    private void clickDate(LocalDate parsedDate, WebElement calendarDiv, LocalDate desiredYearAndMonth) {
-        WebElement datepicker = calendarDiv.findElement(By.cssSelector(".ui-datepicker-calendar"));
-        List<WebElement> daysElements = datepicker.findElements(By.cssSelector("tbody tr td"));
-        daysElements.stream()
-                .filter(day -> Integer.valueOf(day.getAttribute("data-month")).equals(desiredYearAndMonth.getMonthValue() - 1))
-                .map(dayElement -> dayElement.findElement(By.cssSelector("a")))
-                .filter(dayElement -> !dayElement.getText().isEmpty())
-                .filter(dayElement -> Integer.valueOf(dayElement.getText()).equals(parsedDate.getDayOfMonth()))
-                .forEach(WebElement::click);
-    }
-
-    private void openCalendar() {
-        WebElement dateInput = driver.findElement(By.cssSelector("#datepicker"));
-        dateInput.click();
-    }
-
-    private String getActionSelector(LocalDate desirableDate, LocalDate currentDate) {
-        String prevElementSelector = "div .ui-datepicker-prev";
-        String nextElementSelector = "div .ui-datepicker-next";
-
-        if (desirableDate.isBefore(currentDate)) {
-            return prevElementSelector;
-        } else {
-            return nextElementSelector;
-        }
-    }
-
-    private LocalDate getCurrentYearAndMonth(WebElement calendarDiv) {
-        WebElement currentMonthElement = calendarDiv.findElement(By.cssSelector("div .ui-datepicker-month"));
-        WebElement currentYearElement = calendarDiv.findElement(By.cssSelector("div .ui-datepicker-year"));
-
-        return LocalDate.of
-                (Integer.valueOf(currentYearElement.getText()), Month.valueOf(currentMonthElement.getText().toUpperCase()), 1);
+        assertEquals(calendarPage.getInputValue(), expected);
     }
 }
