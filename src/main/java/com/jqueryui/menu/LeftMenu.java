990ce56441d023a694da9c99e707fbad2563b265
jgodwin <jaras48@gmail.com>
2018-10-23 13:22:32 +0200

    LeftMenu impl

---
+++ src/main/java/com/jqueryui/menu/LeftMenu.java
@@ -0,0 +1,37 @@
+package com.jqueryui.menu;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LeftMenu {
+
+    private WebDriver driver;
+
+    @FindBy(xpath = "//*[@id='sidebar']//a[contains(text(), 'Droppable')]")
+    private WebElement droppable;
+
+    @FindBy(xpath = "//*[@id='sidebar']//a[contains(text(), 'Datepicker')]")
+    private WebElement datepicker;
+
+    @FindBy(xpath = "//*[@id='sidebar']//a[contains(text(), 'Slider')]")
+    private WebElement slider;
+
+    public LeftMenu(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickDroppable() {
+        droppable.click();
+    }
+
+    public void clickDatepicker() {
+        datepicker.click();
+    }
+
+    public void clickSlider() {
+        slider.click();
+    }
+}
